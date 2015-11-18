7324324b58cf99975d31d3ccb63b9a53dbcc145f
Tri Nguyen <tringuyenminh92@gmail.com>
2015-11-18 23:12:44 +0700

    using toolbar, sidebar

---
+++ AM.Application/Build/Scripts/Custom/Controller/LandingPageController.js
@@ -0,0 +1,44 @@
+﻿
+
+function LandingPageController($scope) {
+
+    'use strict';
+
+    $scope.landingPageModel = {
+        title: 'Angular Material Layout'
+    };
+
+
+    $scope.menu = [
+    {
+        link: '',
+        title: 'Dashboard',
+        icon: 'dashboard'
+    },
+    {
+        link: '',
+        title: 'Friends',
+        icon: 'group'
+    },
+    {
+        link: '',
+        title: 'Messages',
+        icon: 'message'
+    }
+    ];
+    $scope.admin = [
+      {
+          link: '',
+          title: 'Trash',
+          icon: 'delete'
+      },
+      {
+          link: '',
+          title: 'Settings',
+          icon: 'settings'
+      }
+    ];
+}
+LandingPageController.$inject = ['$scope'];
+angular.module('GlobalModule').controller('LandingPageController', LandingPageController);
+
---
+++ AM.Application/Build/Scripts/Custom/MyApp.js
@@ -0,0 +1,26 @@
+﻿
+(function () {
+
+    'use strict';
+
+    angular.module('GlobalModule', ['ui.router', 'ngMaterial']);
+
+    configFunction.$inject = ['$stateProvider', '$httpProvider', '$locationProvider', '$urlRouterProvider', '$mdThemingProvider'];
+    function configFunction($stateProvider, $httpProvider, $locationProvider, $urlRouterProvider, $mdThemingProvider) {
+
+        $mdThemingProvider.theme('default').primaryPalette('blue', {
+            'default': '800',
+            'hue-1': '500', 
+            'hue-2': '600', 
+            'hue-3': '900'
+        }).accentPalette('green');
+
+        $locationProvider.html5Mode({ enabled: true, requireBase: false });
+
+        $urlRouterProvider.otherwise('/Home');
+    }
+
+
+    angular.module('GlobalModule').config(configFunction);
+
+})();
